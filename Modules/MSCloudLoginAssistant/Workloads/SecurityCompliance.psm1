8ba3be4f71454a390919b37ce01c0137fa94fb64
Vinko Bedek <vinko.bedek@syskit.com>
2021-03-29 11:40:57 +0200

    oauth only as fallback

--- Modules/MSCloudLoginAssistant/Workloads/SecurityCompliance.psm1
+++ Modules/MSCloudLoginAssistant/Workloads/SecurityCompliance.psm1
@@ -40,35 +40,20 @@ function Connect-MSCloudLoginSecurityCompliance
         #     -Verbose:$false -ErrorAction Stop | Out-Null
 
 
-        # there are issues using the official Connect-IPPSSession cmdlet at least when used from Trace, so we do the auth process and connection manually
-        # the issues are most likely because all of the various versions of ADAL(?) between all of the modules used within Trace
-        $pwdCreds = [Microsoft.IdentityModel.Clients.ActiveDirectory.UserPasswordCredential]::new($Global:o365Credential.UserName, $Global:o365Credential.Password)
-        $resourceId = $ConnectionUrl
-        $defaultPsClientId = "fb78d390-0c51-40cd-8e17-fdbfab77341b"
-
-        if ($resourceId -match "ps.compliance.protection")
+        try
         {
-            $uri = [Uri]::new($resourceId)
-            $actualResourceIdHost = $uri.Host -replace "((\w)+\.)?ps.compliance.protection", "ps.compliance.protection"
-            $resourceId = $uri.Scheme + [Uri]::SchemeDelimiter + $actualResourceIdHost
+            $session = Get-SecurityComplianceSessionCoreWithPwd -ConnectionUrl $ConnectionUrl -Credentials $Global:o365Credential
         }
+        catch
+        {
+            if ($_.FullyQualifiedErrorId -ne 'AccessDenied,PSSessionOpenFailed')
+            {
+                throw
+            }
 
-        $task = [Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContextIntegratedAuthExtensions]::AcquireTokenAsync($Global:ADALAppOnBehalfServicePoint.authContext, $resourceId, $defaultPsClientId, $pwdCreds)
-        $task.Wait()
-        $res = $task.Result
-
-        $authHeader = "Bearer $($res.AccessToken)"
-        $scPwd = [securestring]::new()
-        $authHeader.ToCharArray()  | ForEach-Object { $scPwd.AppendChar($_) }
-        $scOAuthCreds = [PSCredential]::new($Global:o365Credential.UserName, $scPwd)
-
-        $connectionUrl = $connectionUrl + "?BasicAuthToOAuthConversion=True"
-        $session = New-PSSession -ConfigurationName "Microsoft.Exchange" `
-            -ConnectionUri $ConnectionUrl `
-            -Credential $scOAuthCreds `
-            -Authentication Basic `
-            -ErrorAction Stop `
-            -AllowRedirection
+            Write-Verbose 'Auth failed, trying via the oauth endpoint'
+            $session = Get-SecurityComplianceSessionCoreWithPwdOauth -ConnectionUrl $ConnectionUrl -Credentials $Global:o365Credential
+        }
 
         $module = Import-PSSession $session  `
             -ErrorAction SilentlyContinue `
@@ -76,4 +61,68 @@ function Connect-MSCloudLoginSecurityCompliance
 
         Import-Module $module -Global | Out-Null
     }
+}
+
+
+function Get-SecurityComplianceSessionCoreWithPwd
+{
+    param(
+        [Parameter(Mandatory = $True)]
+        [String]
+        $ConnectionUrl,
+
+        [Parameter(Mandatory = $True)]
+        [PsCredential]
+        $Credentials
+    )
+
+    $session = New-PSSession -ConfigurationName "Microsoft.Exchange" `
+        -ConnectionUri $ConnectionUrl `
+        -Credential $Credentials `
+        -Authentication Basic `
+        -ErrorAction Stop `
+        -AllowRedirection
+
+    return $session
+}
+
+
+function Get-SecurityComplianceSessionCoreWithPwdOauth
+{
+    param(
+        [Parameter(Mandatory = $True)]
+        [String]
+        $ConnectionUrl,
+
+        [Parameter(Mandatory = $True)]
+        [PsCredential]
+        $Credentials
+    )
+
+    # there are issues using the official Connect-IPPSSession cmdlet at least when used from Trace, so we do the auth process and connection manually
+    # the issues are most likely because all of the various versions of ADAL(?) between all of the modules used within Trace
+    $pwdCreds = [Microsoft.IdentityModel.Clients.ActiveDirectory.UserPasswordCredential]::new($Credentials.UserName, $Credentials.Password)
+    $resourceId = $ConnectionUrl
+    $defaultPsClientId = "fb78d390-0c51-40cd-8e17-fdbfab77341b"
+
+    if ($resourceId -match "ps.compliance.protection")
+    {
+        $uri = [Uri]::new($resourceId)
+        $actualResourceIdHost = $uri.Host -replace "((\w)+\.)?ps.compliance.protection", "ps.compliance.protection"
+        $resourceId = $uri.Scheme + [Uri]::SchemeDelimiter + $actualResourceIdHost
+    }
+
+    $task = [Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContextIntegratedAuthExtensions]::AcquireTokenAsync($Global:ADALAppOnBehalfServicePoint.authContext, $resourceId, $defaultPsClientId, $pwdCreds)
+    $task.Wait()
+    $res = $task.Result
+
+    $authHeader = "Bearer $($res.AccessToken)"
+    $scPwd = [securestring]::new()
+    $authHeader.ToCharArray()  | ForEach-Object { $scPwd.AppendChar($_) }
+    $scOAuthCreds = [PSCredential]::new($Global:o365Credential.UserName, $scPwd)
+
+    $connectionUrl = $connectionUrl + "?BasicAuthToOAuthConversion=True"
+
+    return Get-SecurityComplianceSessionCoreWithPwd -ConnectionUrl $ConnectionUrl -Credentials $scOAuthCreds
+
 }
