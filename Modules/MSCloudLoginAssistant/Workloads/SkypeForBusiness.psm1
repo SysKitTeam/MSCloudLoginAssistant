dcfd340bf85a56161d48baf64ac6e05952e2fc3b
Vinko Bedek <vinko.bedek@syskit.com>
2021-05-23 22:22:16 +0200

    rewrite sfb connection

--- Modules/MSCloudLoginAssistant/Workloads/SkypeForBusiness.psm1
+++ Modules/MSCloudLoginAssistant/Workloads/SkypeForBusiness.psm1
@@ -12,70 +12,46 @@ function Connect-MSCloudLoginSkypeForBusiness
         $Global:o365Credential = Get-Credential -Message "Cloud Credential"
     }
 
-    $userprincipalNameToUse = ""
-    if ($null -eq $Global:o365Credential)
+    Connect-MSCloudLoginTeams
+
+
+    try
     {
-        $userprincipalNameToUse = $global:appIdentityParams.OnBehalfOfUserPrincipalName
+        Enable-AppDomainLoadAnyVersionResolution
+        # if (!('Microsoft.Teams.ConfigApi.Cmdlets.TpmCmdletHost' -as [Type]))
+        # {
+        #     $rootDir = [System.IO.Path]::GetDirectoryName((Get-Module MicrosoftTeams).Path).TrimEnd('\')
+        #     $csAuthCmdlets = $rootDir + "\bin\Microsoft.Teams.ConfigAPI.Cmdlets.private.dll"
+        #     Add-Type -Path $csAuthCmdlets
+        # }
+
+        [SysKit.MsGraphAuthModulePatching.MsTeamsModulePatcher]::DoPatching([SysKit.MsGraphAuthModulePatching.MsTeamsAuthDelegate] {
+
+                #repeating just in case
+                $authResult = Get-OnBehalfOfAuthResult -TargetUri "48ac35b8-9aa8-4d74-927d-1f4a14a0b239" -UserPrincipalName $userprincipalNameToUse
+
+                $result = New-Object 'SysKit.MsGraphAuthModulePatching.MsTeamsAuthResult'
+
+                $teamsEnvironmentName = Get-PsModuleAzureEnvironmentName -AzureCloudEnvironmentName $Global:appIdentityParams.AzureCloudEnvironmentName -Platform "MicrosoftTeams"
+                $configurationApiEnv = [Microsoft.Teams.ConfigApi.Cmdlets.DeploymentConfiguration]::MapTeamsEnvironmentNameToConfigApiEnvironment($teamsEnvironmentName)
+                $configurationApiEndpoint = [Microsoft.Teams.ConfigApi.Cmdlets.DeploymentConfiguration]::ConfigApiEnvironmentToEndpoints[$configurationApiEnv]
+
+                $administeredDomain = $authResult.UserInfo.DisplayableId.Split('@')[1]
+
+                $result.AccessToken = $authResult.AccessToken
+                $result.UserName = $authResult.UserInfo.DisplayableId
+                $result.UniqueId = $authResult.UserInfo.UniqueId
+                $result.TenantId = $authResult.TenantId
+                $result.AdministeredDomain = $administeredDomain
+                $result.ConfigApiEnvironment = $configurationApiEnv
+                $result.ConfigApiEndpoint = $configurationApiEndpoint
+
+                return $result
+            })
     }
-    else
+    finally
     {
-        $userprincipalNameToUse = $Global:o365Credential.UserName
+        Disable-AppDomainLoadAnyVersionResolution
     }
 
-    $adminDomain = $userprincipalNameToUse.Split('@')[1]
-    if ($userprincipalNameToUse.Split('@')[1] -like '*.de')
-    {
-        $Global:CloudEnvironment = 'Germany'
-        Write-Warning 'Microsoft Teams is not supported in the Germany Cloud'
-        return
-    }
-
-
-    # Skype for Business actually has a cool cmdlet inside of the MicrosoftTeams module named New-CsOnlineSession
-    # unfortunately I could not get it to work with the application identity so we remain like this
-    # There is probably a way to achieve it with the application identity but I do not want to risk breaking things, atleast for the time being
-    # hope to revisit it in the future
-
-    $maxConnectionsSearchString = "The maximum number of concurrent shells"
-    
-    Ensure-RemotePsSession -RemoteSessionName "Skype For Business" `
-        -TestModuleLoadedCommand "Get-CsTeamsClientConfiguration" `
-        -MaxConnectionsMessageSearchString $maxConnectionsSearchString `
-        -ExistingSessionPredicate { $_.Name -like 'SfBPowerShellSession*' } `
-        -MaxAttempts 15 `
-        -CreateSessionScriptBlock {
-
-        $ErrorActionPreference = "Stop"
-
-        $targetUri = Get-SkypeForBusinessServiceEndpoint -TargetDomain $adminDomain
-
-        # we don't call Get-SkypeForBusinessAccessInfo
-        # in the application identity use case we have our own clientId
-        # disregarded the $authuri for now since it would mean that the authentication context would not be global any more
-        $AccessToken = Get-OnBehalfOfAccessToken -TargetUri $targetUri -UserPrincipalName $userprincipalNameToUse
-
-        $networkCreds = [System.Net.NetworkCredential]::new("", $AccessToken)
-        $secPassword = $networkCreds.SecurePassword
-        $user = "oauth"
-        $cred = [System.Management.Automation.PSCredential]::new($user, $secPassword)
-
-        $queryStr = "AdminDomain=$adminDomain"
-
-        $ConnectionUri = [UriBuilder]$targetUri
-        $ConnectionUri.Query = $queryStr
-
-        $psSessionName = "SfBPowerShellSession"
-        $ConnectorVersion = "7.0.2374.2"
-        $SessionOption = New-PsSessionOption
-        $SessionOption.ApplicationArguments = @{}
-        $SessionOption.ApplicationArguments['X-MS-Client-Version'] = $ConnectorVersion
-        $SessionOption.NoMachineProfile = $true
-
-
-        # leaving the global variables just in case some cmdlet uses them
-        $Global:SkypeSession = New-PSSession -Name $psSessionName -ConnectionUri $ConnectionUri.Uri `
-            -Credential $cred -Authentication Basic -SessionOption $SessionOption
-        $Global:SkypeModule = Import-PSSession $Global:SkypeSession
-        Import-Module $Global:SkypeModule -Global | Out-Null
-    }
 }
