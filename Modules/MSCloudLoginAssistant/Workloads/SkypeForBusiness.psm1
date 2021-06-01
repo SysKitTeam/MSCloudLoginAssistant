d2ed58e07fa7aef25cc97cdc71d5078be4643877
Vinko Bedek <vinko.bedek@syskit.com>
2021-06-01 09:02:52 +0200

    Sfb: new auth patch dll and removed assembly redirect because of issues

--- Modules/MSCloudLoginAssistant/Workloads/SkypeForBusiness.psm1
+++ Modules/MSCloudLoginAssistant/Workloads/SkypeForBusiness.psm1
@@ -12,46 +12,30 @@ function Connect-MSCloudLoginSkypeForBusiness
         $Global:o365Credential = Get-Credential -Message "Cloud Credential"
     }
 
+    # the Teams module supports Sfb remote connections so we must first connect to it
+    # but the thing is, it does not support connections with application permissions so we must do some magic
+    # and patch the auth process to provide our own access token on behalf of the service user
     Connect-MSCloudLoginTeams
 
 
-    try
-    {
-        Enable-AppDomainLoadAnyVersionResolution
-        # if (!('Microsoft.Teams.ConfigApi.Cmdlets.TpmCmdletHost' -as [Type]))
-        # {
-        #     $rootDir = [System.IO.Path]::GetDirectoryName((Get-Module MicrosoftTeams).Path).TrimEnd('\')
-        #     $csAuthCmdlets = $rootDir + "\bin\Microsoft.Teams.ConfigAPI.Cmdlets.private.dll"
-        #     Add-Type -Path $csAuthCmdlets
-        # }
+    # patching is performed via a custom dll that uses Harmony to change the auth behaviour
+    [SysKit.MsGraphAuthModulePatching.MsTeamsModulePatcher]::DoPatching([SysKit.MsGraphAuthModulePatching.MsTeamsAuthDelegate] {
+            $authResult = Get-OnBehalfOfAuthResult -TargetUri "48ac35b8-9aa8-4d74-927d-1f4a14a0b239" -UserPrincipalName $userprincipalNameToUse
+            $result = New-Object 'SysKit.MsGraphAuthModulePatching.MsTeamsAuthResult'
 
-        [SysKit.MsGraphAuthModulePatching.MsTeamsModulePatcher]::DoPatching([SysKit.MsGraphAuthModulePatching.MsTeamsAuthDelegate] {
+            $teamsEnvironmentName = Get-PsModuleAzureEnvironmentName -AzureCloudEnvironmentName $Global:appIdentityParams.AzureCloudEnvironmentName -Platform "MicrosoftTeams"
+            $configurationApiEnv = [Microsoft.Teams.ConfigApi.Cmdlets.DeploymentConfiguration]::MapTeamsEnvironmentNameToConfigApiEnvironment($teamsEnvironmentName)
+            $configurationApiEndpoint = [Microsoft.Teams.ConfigApi.Cmdlets.DeploymentConfiguration]::ConfigApiEnvironmentToEndpoints[$configurationApiEnv]
+            $administeredDomain = $authResult.UserInfo.DisplayableId.Split('@')[1]
 
-                #repeating just in case
-                $authResult = Get-OnBehalfOfAuthResult -TargetUri "48ac35b8-9aa8-4d74-927d-1f4a14a0b239" -UserPrincipalName $userprincipalNameToUse
+            $result.AccessToken = $authResult.AccessToken
+            $result.UserName = $authResult.UserInfo.DisplayableId
+            $result.UniqueId = $authResult.UserInfo.UniqueId
+            $result.TenantId = $authResult.TenantId
+            $result.AdministeredDomain = $administeredDomain
+            $result.ConfigApiEnvironment = $configurationApiEnv
+            $result.ConfigApiEndpoint = $configurationApiEndpoint
 
-                $result = New-Object 'SysKit.MsGraphAuthModulePatching.MsTeamsAuthResult'
-
-                $teamsEnvironmentName = Get-PsModuleAzureEnvironmentName -AzureCloudEnvironmentName $Global:appIdentityParams.AzureCloudEnvironmentName -Platform "MicrosoftTeams"
-                $configurationApiEnv = [Microsoft.Teams.ConfigApi.Cmdlets.DeploymentConfiguration]::MapTeamsEnvironmentNameToConfigApiEnvironment($teamsEnvironmentName)
-                $configurationApiEndpoint = [Microsoft.Teams.ConfigApi.Cmdlets.DeploymentConfiguration]::ConfigApiEnvironmentToEndpoints[$configurationApiEnv]
-
-                $administeredDomain = $authResult.UserInfo.DisplayableId.Split('@')[1]
-
-                $result.AccessToken = $authResult.AccessToken
-                $result.UserName = $authResult.UserInfo.DisplayableId
-                $result.UniqueId = $authResult.UserInfo.UniqueId
-                $result.TenantId = $authResult.TenantId
-                $result.AdministeredDomain = $administeredDomain
-                $result.ConfigApiEnvironment = $configurationApiEnv
-                $result.ConfigApiEndpoint = $configurationApiEndpoint
-
-                return $result
-            })
-    }
-    finally
-    {
-        Disable-AppDomainLoadAnyVersionResolution
-    }
-
+            return $result
+        })
 }
